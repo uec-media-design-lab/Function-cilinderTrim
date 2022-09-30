5c4b874f6f88d543ec8b04bad502420ed9dfb325
Ashroom66 <ashroom09@gmail.com>
2022-09-30 13:25:29 +0900

    feat: minimal vec3 class for sampling rand in cilinder

---
+++ vec3.h
@@ -0,0 +1,43 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <cmath>
+#include <iostream>
+
+using std::sqrt;
+
+class vec3 {
+    public:
+    vec3() : e{0,0,0} {}
+    vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+
+    double x() const {return e[0];}
+    double y() const {return e[1];}
+    double z() const {return e[2];}
+    inline static vec3 random() {
+        return vec3(random_double(),
+                    random_double(),
+                    random_double());
+    }
+    inline static vec3 random(double min, double max) {
+        return vec3(random_double(min,max),
+                    random_double(min,max),
+                    random_double(min,max));
+    }
+
+    public:
+    double e[3];
+};
+vec3 random_in_unit_disk() {
+    while (true) {
+        auto p = vec3(random_double(-1, 1), random_double(-1, 1), 0);
+        if (p.length_squared()>=1) continue;
+        return p;
+    }
+}
+vec3 random_in_unit_cilinder() {
+    vec3 p = random_in_unit_disk();
+    p.z() = random_double();
+    return p;
+}
+#endif
